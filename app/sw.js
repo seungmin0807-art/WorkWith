5b11fba0b06cc979b045fa98bb2865b741a31802
seungmin0807-art <seungmin0807@snu.ac.kr>
2026-04-22 03:30:22 +0900

    Remove home logo glow

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "workwith-ios-pwa-v16";
+const CACHE_VERSION = "workwith-ios-pwa-v17";
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
